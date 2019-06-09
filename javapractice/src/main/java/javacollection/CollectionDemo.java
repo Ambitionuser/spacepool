684319ac89de8ee3181af1eb17f06b437daa53e5
Administrator <792342548@qq.com>
2019-06-09 23:04:51 +0800

    1.添加集合的练习

---
+++ javapractice/src/main/java/javacollection/CollectionDemo.java
@@ -0,0 +1,9 @@
+package javacollection;
+
+import java.util.ArrayList;
+
+public class CollectionDemo {
+    public static void main(String[] args) {
+        ArrayList<Object> arrayList = new ArrayList<Object>();
+    }
+}
