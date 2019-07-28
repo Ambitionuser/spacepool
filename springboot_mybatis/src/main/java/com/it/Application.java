9ae4d6babf3b711e7048687e44223e209670c5b4
Administrator <792342548@qq.com>
2019-07-28 23:01:41 +0800

    添加插件

--- springboot_mybatis/src/main/java/com/it/Application.java
+++ springboot_mybatis/src/main/java/com/it/Application.java
@@ -1,12 +1,13 @@
 package com.it;
 
-import com.it.controller.EcmdbBatch;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @MapperScan("com.it.dao")
+@EnableCaching
 public class Application {
 
 	public static void main(String[] args) {
